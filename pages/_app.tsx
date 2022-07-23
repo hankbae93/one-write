b2b975e89227e3c8293b5f77c0eb90b73ff17195
Hank <77097180+ranjafunc@users.noreply.github.com>
2022-07-23 22:12:13 +0900

    Update react-query deprecated

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,20 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+import { RecoilRoot } from 'recoil';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const queryClient = new QueryClient();
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RecoilRoot>
+        <Component {...pageProps} />
+        <ReactQueryDevtools />
+      </RecoilRoot>
+    </QueryClientProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
