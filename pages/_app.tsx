eb05fb389d8281128093908c88b47e5500d40f3e
Hank <77097180+ranjafunc@users.noreply.github.com>
2022-07-24 15:39:58 +0900

    [ Settings ] styled-components theme setting

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,24 @@
 import type { AppProps } from 'next/app';
+import { ThemeProvider } from 'styled-components';
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-
 import { RecoilRoot } from 'recoil';
+import { theme } from '@styles/theme';
+import GlobalStyle from '@styles/global-style';
 
 function MyApp({ Component, pageProps }: AppProps) {
   const queryClient = new QueryClient();
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <RecoilRoot>
-        <Component {...pageProps} />
-        <ReactQueryDevtools />
-      </RecoilRoot>
-    </QueryClientProvider>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <QueryClientProvider client={queryClient}>
+        <RecoilRoot>
+          <Component {...pageProps} />
+          <ReactQueryDevtools />
+        </RecoilRoot>
+      </QueryClientProvider>
+    </ThemeProvider>
   );
 }
 
